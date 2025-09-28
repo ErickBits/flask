3390b327e680245c89f4690017417b853b686184
ErickBit <44269535+ErickBits@users.noreply.github.com>
2025-09-27 22:12:45 -0500

    learning how to add renders

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, my Flask!"
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f"Hello, {name}!"
+
+@app.route('/square/<int:number>')
+def square(number):
+    return str(number * number)
+
+@app.route('/mostrar')
+def mostrar():
+    return render_template('mostrar.html', name="Flask User")  
