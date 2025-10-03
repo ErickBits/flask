8ff2e4da282fed4ffd71bc1d6a26bce7c0b1fc4f
ErickBit <44269535+ErickBits@users.noreply.github.com>
2025-10-02 23:52:04 -0500

    new functions, routes and features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect, url_for, abort
 
 app = Flask(__name__)
 
@@ -17,3 +17,20 @@ def square(number):
 @app.route('/mostrar')
 def mostrar():
     return render_template('mostrar.html', name="Flask User")  
+
+@app.route('/redirect')
+def redirect_example():
+    return redirect(url_for('home'))   
+
+@app.route('/error')
+def error():
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', error=e), 404
+
+@app.route("/hola/<name>")
+@app.route("/saludar/<name>")
+def hola(name):
+    return render_template("hola.html", name=name)
